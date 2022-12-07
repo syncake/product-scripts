b9bdb9f7f87ed897e96f680bdfd40c40eb5d44dd
jack <jack@mbp>
2022-12-08 03:03:02 +0800

    backup script added

---
+++ mysql-backup-remotelly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hostname="web"
+hostuser="root"
+dbname="hadoken"
+path_local="/opt/db/"
+
+#######################
+tmp_zipfile="/tmp/d.sql.gz"
+
+ssh -n $hostuser@$hostname "mysqldump ${dbname} | gzip > ${tmp_zipfile}"
+if [[ $? -ne 0 ]]; then
+    echo "backup remotelly failed"
+    exit 1
+fi
+
+mkdir -p $path_local
+scp -rp $hostuser@$hostname:${tmp_zipfile} ${path_local}/${dbname}-$(date +"%Y%m%d-%H%M%S").sql.gz
+
+ssh -n $hostuser@$hostname "rm -f ${tmp_zipfile}"
