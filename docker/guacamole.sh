dfc3e83453cf30e853c04c3a7e9bda345fdc9ba9
jojo <jojo@mbp>
2023-10-31 08:03:12 +0800

    update gua script

--- docker/guacamole.sh
+++ docker/guacamole.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 docker run --name guacd \
+    --restart unless-stopped \
     -d -p 127.0.0.1:4822:4822 guacamole/guacd
+docker cp /usr/share/fonts/truetype guacd:/usr/share/fonts/
 
 docker run --name guacamole \
     --link guacd:guacd  \
+	--restart unless-stopped \
     -e MYSQL_HOSTNAME=172.17.0.1  \
     -e MYSQL_PORT=3306  \
     -e MYSQL_DATABASE=guacd  \
