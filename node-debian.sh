61e7110d526e42544d7ebab7a0c96048ca4dc3b8
jojo <jojo@mbp>
2024-01-12 05:02:14 +0800

    node @debian auto-installing script

--- node-debian.sh
+++ node-debian.sh
@@ -5,6 +5,6 @@ node_mainversion="14"
 apt update -y
 apt install npm -y 
 npm i -g n
-n $(n lsr 14 | head -2 | tail -1)
+n $(n lsr $node_mainversion | head -2 | tail -1)
 npm i -g yarn 
 apt remove nodejs npm -y --purge
